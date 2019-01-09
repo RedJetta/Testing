28b8f2077b9eb9d3e0fe771c9c6b192108772250
Ваше Имя <mms98@inbox.ru>
2019-01-09 17:52:38 +0300

    New

---
+++ Framework/src/test/java/ru/aviaYandex/BaseTest.java
@@ -0,0 +1,31 @@
+package ru.aviaYandex;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void initialDriver(){
+        System.setProperty("webdriver.chrome.driver", "/home/maks/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterClass
+    public static void quit(){
+        driver.quit();
+    }
+}
---
+++ Framework/src/test/java/ru/aviaYandex/FirstTest.java
@@ -0,0 +1,58 @@
+package ru.aviaYandex;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
+
+
+import static org.junit.Assert.*;
+
+public class FirstTest extends BaseTest {
+
+    public static Search search;
+
+    @BeforeClass
+    public static void initial() {
+        search = new Search(driver);
+    }
+
+    @Test
+    public void ticketsSearchWithValidData() throws InterruptedException {
+        driver.get("https://avia.yandex.ru");
+        search.typeInputFrom("Москва");
+        search.typeInputTo("Санкт-Питербург");
+        search.clickOnCalendar();
+        search.selectDepartureDate("13", "08", "2018");
+        search.selectArrivalDate("14", "08", "2018");
+        search.clickOnPeople();
+        search.addAdults(2);
+        search.clickOnSearch();
+    }
+
+    @Test
+    public void invalidFromAndToFields(){
+        driver.get("https://avia.yandex.ru");
+        search.typeInputTo("Менск");
+        search.typeInputFrom("На Берлин!");
+        search.clickOnSearch();
+        String title = driver.getTitle();
+        assertEquals(title, "Неверно введены данные");
+    }
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void pastDate(){
+        driver.get("https://avia.yandex.ru");
+        search.clickOnCalendar();
+        search.selectDepartureDate("10", "12", "1945");
+
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void futureDate(){
+        driver.get("https://avia.yandex.ru");
+        search.clickOnCalendar();
+        search.selectDepartureDate("31", "10", "2100");
+    }
+
+}
+
